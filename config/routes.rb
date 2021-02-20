e0a4cd2caa0d5769aec9dd9d240a7c5947ba1294
leellou <lea.destaillac@gmail.com>
2021-02-20 12:49:15 +0100

    creation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :participations, only: [ :create ]
+  resources :games, only: [ :show, :new, :create ] do
+    resources :participations, only: [ :edit, :update ]
+  end
+  resources :user_providers, only: [ :new, :create ]
 end
