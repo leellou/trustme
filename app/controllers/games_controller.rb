7a1dc00f072c2ca40893d261425b3b570fb5c340
leellou <lea.destaillac@gmail.com>
2021-02-23 20:37:56 +0100

    participation new/create token(en cours)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,16 +9,15 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    byebug
     @game.user = current_user
-    @game.token = Devise.friendly_token.first(6) 
+    @game.token = Devise.friendly_token.first(6)
     @game.save
     redirect_to root_path
   end
 
   def game_params
       params.require(:game).permit(:user, :movie, :type, :genre, :year,
-                                  :director, :language, :language_subtitle, 
+                                  :director, :language, :language_subtitle,
                                   :country, :vote_average, :token)
   end
 
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,4 +1,5 @@
 class ParticipationsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
 
   def new
     @participation = Participation.new
@@ -7,11 +8,19 @@ class ParticipationsController < ApplicationController
   def create
     @participation = Participation.new(participation_params)
     @participation.user = current_user
+    @game = Game.find_by(token: params[:token])
+    @participation.game = @game
 
     if @participation.save
-      redirect_to game_path(game)
+      redirect_to game_path(@game)
     else
       render :new
     end
+  end
+
+  private
+
+  def participation_params
+    params.require(:participation).permit(:type, :genre, :year, :director, :language, :language_subtitle, :country, :vote_average)
   end
 end
