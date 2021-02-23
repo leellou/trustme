24255826ef45ab34db51165cd436e46167e3ff7b
leellou <lea.destaillac@gmail.com>
2021-02-23 21:04:39 +0100

    participation create/new with token

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,12 +1,12 @@
 class ParticipationsController < ApplicationController
-  # skip_before_action :verify_authenticity_token
 
   def new
     @participation = Participation.new
   end
 
   def create
-    @participation = Participation.new(participation_params)
+
+    @participation = Participation.new
     @participation.user = current_user
     @game = Game.find_by(token: params[:token])
     @participation.game = @game
@@ -18,7 +18,7 @@ class ParticipationsController < ApplicationController
     end
   end
 
-  private
+private
 
   def participation_params
     params.require(:participation).permit(:type, :genre, :year, :director, :language, :language_subtitle, :country, :vote_average)
