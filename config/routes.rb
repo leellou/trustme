40a4f7d0a6156d7d23cebcfc44d7322465583b3b
leellou <lea.destaillac@gmail.com>
2021-02-23 20:52:26 +0100

    create user ctrl

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
+  resources :users, :only => [:show]
   root to: 'pages#home'
   resources :participations, only: [ :create ]
   resources :games, only: [ :show, :new, :create ] do
